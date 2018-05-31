26fed90db214c0e0b223b6854faeb9d6f0fc1068
tushmesh <39365355+tushmesh@users.noreply.github.com>
2018-05-31 05:18:40 +0000

    Updated

--- fetch_delta.ps1
+++ fetch_delta.ps1
@@ -1,12 +1,15 @@
-$str=Get-Content "C:\Users\tushar.u.meshram\OneDrive - Accenture\SSE\Document\Release_Notes.txt" -raw
-$str | Select-String -Pattern '(?ms)CHANGED_FILES_START(.*?)CHANGED_FILES_END' -AllMatches | 
-foreach 
-     {
-          $_.Matches | foreach {
-          $_.Groups[0].Value
-          }
-     } | out-file files_to_pkg.txt
-get-content .\files_to_pkg.txt  | foreach-object {$_ -replace "CHANGED_FILES_START",""} | foreach-object {$_ -replace "CHANGED_FILES_END",""} | out-file files_to_pkg1.txt  
+$str=Get-Content "$ENV:WORKSPACE\Release_Notes\Release_Notes.txt" -raw
+$str | Select-String -Pattern '(?ms)CHANGED_FILES_START(.*?)CHANGED_FILES_END' -AllMatches | foreach {$_.Matches | foreach {$_.Groups[0].Value}} | out-file $ENV:WORKSPACE\Logs\files_to_pkg.txt
+
+Get-Content $ENV:WORKSPACE\Logs\files_to_pkg.txt  | foreach-object {$_ -replace "CHANGED_FILES_START",""} | foreach-object {$_ -replace "CHANGED_FILES_END",""} | out-file $ENV:WORKSPACE\Logs\files_to_pkg1.txt
 ## Remove Blank Lines
-get-content .\files_to_pkg1.txt | ? {$_.trim() -ne "" } | set-content  .\files_to_pkg2.txt
-     
+
+Get-Content $ENV:WORKSPACE\Logs\files_to_pkg1.txt | ? {$_.trim() -ne "" } | set-content  $ENV:WORKSPACE\Logs\files_to_pkg2.txt
+foreach ( $exec in Get-Content $ENV:WORKSPACE\Logs\files_to_pkg2.txt ) 
+{
+     Copy-Item -Path $exec -Destination C:\Tushar\ 
+}
+
+Compress-Archive -Path C:\Tushar\* -DestinationPath C:\Tushar\Bundle.zip
+    
+    
